4ca6f3410e48fcd7df32bfe5de32213d1eca0800
Pablo Lichtig <pablo.lichtig@gmail.com>
2025-06-15 10:48:52 -0300

    Add basic error handling and --verbose option

--- main.py
+++ main.py
@@ -7,17 +7,32 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key=api_key)
 
 if len(sys.argv) < 2:
-    raise IOError('No prompt has been provided')
+    raise IOError("No prompt has been provided")
+
+user_prompt = sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
 responses = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=sys.argv[1],
+    contents=messages,
 )
 
+
+prompt_tokens = responses.usage_metadata.prompt_token_count
+response_tokens = responses.usage_metadata.candidates_token_count
 print(responses.text)
-print(f"Prompt tokens: {responses.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {responses.usage_metadata.candidates_token_count}")
+# print(f"Prompt tokens: {prompt_tokens}")
+# print(f"Response tokens: {response_tokens}")
+
+if "--verbose" in sys.argv:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
